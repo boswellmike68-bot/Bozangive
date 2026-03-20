3c584bcc50a00a209f4110a17710ce09a175820d
Mike Boswell <boswellmike68@gmail.com>
2026-03-20 13:48:49 -0400

    Update repoCrawler.js

--- github/repoCrawler.js
+++ github/repoCrawler.js
@@ -1,1 +1,17 @@
+import { Octokit } from "octokit";
+import fs from "fs";
 
+export function createGitHubClient() {
+  const token = process.env.BOZANGIVE_TOKEN;
+
+  if (!token) {
+    throw new Error("Missing BOZANGIVE_TOKEN environment variable.");
+  }
+
+  return new Octokit({
+    auth: token,
+    request: {
+      fetch: (...args) => fetch(...args)
+    }
+  });
+}
