fc17bcde01d42436cec57018e9a21976c8169d8a
Mike Boswell <boswellmike68@gmail.com>
2026-03-20 13:44:24 -0400

    Update readOnlyGuard.js

--- core/readOnlyGuard.js
+++ core/readOnlyGuard.js
@@ -1,1 +1,8 @@
-
+export function readOnlyGuard(task, permissions) {
+  if (permissions.mode === "read-only") {
+    if (task.type === "write" || task.type === "modify" || task.type === "delete") {
+      return false;
+    }
+  }
+  return true;
+}
