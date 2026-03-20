f6d2e9d237906b357413b96c3839939f2a5eaafc
Mike Boswell <boswellmike68@gmail.com>
2026-03-20 13:43:52 -0400

    Update approvalGate.js

--- core/approvalGate.js
+++ core/approvalGate.js
@@ -1,1 +1,9 @@
+export function approvalGate(task, supervision) {
+  if (!supervision.requires_owner_approval) return true;
 
+  if (task.approvedBy !== supervision.owner_id) {
+    return false;
+  }
+
+  return true;
+}
