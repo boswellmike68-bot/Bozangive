9af85be8947a5e48e08c435fdb4a498f41ba4f88
Mike Boswell <boswellmike68@gmail.com>
2026-03-20 13:44:52 -0400

    Update taskRouter.js

--- core/taskRouter.js
+++ core/taskRouter.js
@@ -1,1 +1,19 @@
+import { crawlRepo } from "../github/repoCrawler.js";
+import { extractMarkdown } from "../github/mdExtractor.js";
+import { classifyFiles } from "../github/fileClassifier.js";
+import { generateSummary } from "../github/summaryGenerator.js";
 
+export async function routeTask(task) {
+  switch (task.action) {
+    case "crawl":
+      return await crawlRepo(task.repo);
+    case "extract_md":
+      return await extractMarkdown(task.repo);
+    case "classify":
+      return await classifyFiles(task.repo);
+    case "summarize":
+      return await generateSummary(task.repo);
+    default:
+      return { status: "error", message: "Unknown task action." };
+  }
+}
